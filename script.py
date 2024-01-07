bf54d39ba3edf1508520334c5a9f041593cb0ad5
Copcescki <carlos.copcescki@gmail.com>
2024-01-07 17:31:16 -0300

    Adicionado gráfico de rendimento de ativos

--- script.py
+++ script.py
@@ -24,31 +24,56 @@ de_data = st.sidebar.date_input("De:", data_inicio)
 para_data = st.sidebar.date_input("Para:", data_final)
 para_data_correta = para_data + timedelta(1)
 
+selected_indice = st.sidebar.selectbox("Selecione um indice para comparar", [''] + lista_indices_select)
 # Condição para evitar conflito de datas
 
 if de_data > para_data:
     st.sidebar.warning("A data de inicio não pode ser superior a data atual")
 
 # Coletar dados para os ativos selecionados
+mapa_indices = {
+    'BOVESPA': '^BVSP',
+    'OURO': 'GC=F',
+    'DOW JONES': '^DJI',
+    'S&P 500': '^GSPC',
+    'NASDAQ': 'NQ=F'
+}
+
 dados_ativos = {}
+
+selected_ativos.append(selected_indice)
+
 for ativo in selected_ativos:
-    call_api = yf.Ticker(f"{ativo}.SA").history(start=f"{de_data}", end=f"{para_data_correta}")
+    if ativo in mapa_indices:
+        # Se o ativo estiver no mapa_indices, use o valor mapeado diretamente
+        symbol = mapa_indices[ativo]
+    else:
+        # Caso contrário, acrescente o sufixo ".SA"
+        symbol = f"{ativo}.SA"
+    
+    # Chamar a API com o símbolo obtido
+    call_api = yf.Ticker(symbol).history(start=f"{de_data}", end=f"{para_data_correta}")
+    
+    # Adicionar os dados ao dicionário
     dados_ativos[ativo] = pd.DataFrame(call_api)
     
 # Formatar coluna de datas
 for ativo, df in dados_ativos.items():
-    df.index = df.index.strftime('%Y-%m-%d')
+    # Verificar se o índice é do tipo datetime antes de formatar
+    if isinstance(df.index, pd.DatetimeIndex):
+        df.index = df.index.strftime('%Y-%m-%d')
 
 # Elaborando o dash
 st.title("Monitoramento de Análise Financeira")
-st.subheader("Comparação de cotação - Visualização gráfica")
+st.subheader("Cotação de ativos")
 
 # Plotando o gráfico de cotações
 fig_cotacoes, ax_cotacoes = plt.subplots(figsize=(12, 6))
 
 for ativo, df in dados_ativos.items():
-    ax_cotacoes.plot(df.index, df['Close'], label=ativo)
-
+    # Ignorar ativos que são iguais aos índices do mapa_indices
+    if ativo not in lista_indices_select:
+        ax_cotacoes.plot(df.index, df['Close'], label=ativo)
 # Adicionando legenda e título
 plt.legend()
 plt.title("Comparação de Cotações de Ativos")
@@ -59,28 +84,35 @@ plt.ylabel('Preço de Fechamento')
 st.pyplot(fig_cotacoes)
 
 # Plotando o gráfico de retornos
-st.subheader("Comparação de rendimento - Visualização gráfica")
-fig_retornos, ax_retornos = plt.subplots(figsize=(12, 6))
-dados_retornos_completo = {}
-
-for ativo, df in dados_ativos.items():
-    # Calcular os retornos para cada ativo
-    df_retornos = (df['Close'].pct_change() + 1).cumprod() - 1
-    dados_retornos_completo[ativo] = df_retornos  
-    ax_retornos.plot(df_retornos.index, df_retornos, label=f"{ativo}")
-
-# Adicionando legenda e título
-plt.legend()
-plt.title("Comparação de Rendimento de Ativos")
-plt.xlabel('Data')
-plt.ylabel('Rendimento')
-
-# Exibindo o gráfico de retornos
-st.pyplot(fig_retornos)
+if selected_indice == "":
+    st.warning("Selecione o indice para analisar o rendimento")
+else:
+    st.subheader("Rendimento de ativos")
+    fig_retornos, ax_retornos = plt.subplots(figsize=(12, 6))
+    dados_retornos_completo = {}
+    for ativo, df in dados_ativos.items():
+        # Calcular os retornos para cada ativo
+        df_retornos = (df['Close'].pct_change() + 1).cumprod() - 1
+        dados_retornos_completo[ativo] = df_retornos  
+        ax_retornos.plot(df_retornos.index, df_retornos, label=f"{ativo}")       
+        # Adicionando legenda e título
+        plt.legend()
+        plt.title("Comparação de Rendimento de Ativos")
+        plt.xlabel('Data')
+        plt.ylabel('Rendimento')
+   # Exibindo o gráfico de retornos
+    st.pyplot(fig_retornos)
 
 # Definir cores de rendimento positivo ou negativo
 color_positive = 'green'
 color_negative = 'red'
+
+
+# Remover o índice selecionado se estiver presente
+if selected_indice in dados_ativos:
+    del dados_ativos[selected_indice]
+
+selected_ativos.append(selected_indice)
 
 # Exibindo a tabela com os dados
 st.subheader("Dados dos Ativos")
@@ -88,15 +120,20 @@ for ativo, df in dados_ativos.items():
     st.write(f"Dados para {ativo}")
     st.dataframe(df, width=850, height=350)
     last_data = df.iloc[-1]
-    rendimento_total = df_retornos.iloc[-1] if len(df) > 1 else 0
-
-    st.write(f"**Alta do último dia disponível:** R$ {last_data['High']:.2f}")
-    st.write(f"**Baixa do último dia disponível:** R$ {last_data['Low']:.2f}")
-    st.write(f"**Fechamento do último dia disponível:** R$ {last_data['Close']:.2f}")
+    
+    # Calcular os retornos apenas se houver dados disponíveis
+    if len(df) > 1:
+        df_retornos = (df['Close'].pct_change() + 1).cumprod() - 1
+        rendimento_total = df_retornos.iloc[-1]
+        
+        st.write(f"**Alta do último dia disponível:** R$ {last_data['High']:.2f}")
+        st.write(f"**Baixa do último dia disponível:** R$ {last_data['Low']:.2f}")
+        st.write(f"**Fechamento do último dia disponível:** R$ {last_data['Close']:.2f}")
+        if rendimento_total < 0:
+            st.write(f"**Rendimento:** <span style='color:{color_negative}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
+        else:
+            st.write(f"**Rendimento:** <span style='color:{color_positive}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
+    else:
+        st.write("Não há dados suficientes para calcular retornos.")
     
     st.write("\n---\n")
-
-if rendimento_total < 0:
-    st.sidebar.write(f"**Rendimento:** <span style='color:{color_negative}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
-else:
-    st.sidebar.write(f"**Rendimento:** <span style='color:{color_positive}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
