de48155d6ccb55ca2afcb86747e9d6def3d9e350
Copcescki <carlos.copcescki@gmail.com>
2024-01-10 00:50:14 -0300

    Adição de IPCA, CDI e TAXA SELIC

--- script.py
+++ script.py
@@ -3,12 +3,13 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+from bcb import sgs
 
 # Lista de ativos utilizados
 lista = list(pd.read_excel('listativos.xls')['Código'].values)
 lista.sort()
 lista_ativos = [ativo + '.SA' for ativo in lista]
-lista_indices_select = ['BOVESPA', 'OURO', 'DOW JONES', 'S&P 500', 'NASDAQ']
+lista_indices_select = ['CDI', 'IPCA', 'TAXA SELIC', 'BOVESPA']
 
 # Definir intervalo de datas
 data_inicio = datetime.today() - timedelta(30)
@@ -25,6 +26,27 @@ para_data = st.sidebar.date_input("Para:", data_final)
 para_data_correta = para_data + timedelta(1)
 
 selected_indice = st.sidebar.selectbox("Selecione um indice para comparar", [''] + lista_indices_select)
+#Importar dados de indices
+
+ipca_dados = sgs.get(('ipca', 433), start=de_data, end=para_data_correta)
+selic_dados = sgs.get(('selic', 432), start=de_data, end=para_data_correta)
+cdi_dados = sgs.get(('cdi', 12), start=de_data, end=para_data_correta)
+
+try:
+    # Tentar ler os dados 
+    df_ipca = pd.DataFrame(ipca_dados)
+    df_cdi = pd.DataFrame(cdi_dados)
+    df_selic = pd.DataFrame(selic_dados)
+except ValueError as e:     
+    st.error(f"Erro ao ler os dados do IPCA: {e}")
+    df_ipca = pd.DataFrame()
+    df_cdi = pd.DataFrame()
+    df_selic = pd.DataFrame()
+
+df_retornos_ipca = (df_ipca['ipca'].pct_change() + 1).cumprod() - 1
+df_retornos_cdi = (df_cdi['cdi'].pct_change() + 1).cumprod() - 1
+df_retornos_selic = (df_selic['selic'].pct_change() + 1).cumprod() - 1
+
 # Condição para evitar conflito de datas
 
 if de_data > para_data:
@@ -33,10 +55,6 @@ if de_data > para_data:
 # Coletar dados para os ativos selecionados
 mapa_indices = {
     'BOVESPA': '^BVSP',
-    'OURO': 'GC=F',
-    'DOW JONES': '^DJI',
-    'S&P 500': '^GSPC',
-    'NASDAQ': 'NQ=F'
 }
 
 dados_ativos = {}
@@ -85,21 +103,30 @@ st.pyplot(fig_cotacoes)
 
 # Plotando o gráfico de retornos
 if selected_indice == "":
-    st.warning("Selecione o indice para analisar o rendimento")
+    st.warning("Selecione o índice para analisar o rendimento")
 else:
     st.subheader("Rendimento de ativos")
     fig_retornos, ax_retornos = plt.subplots(figsize=(12, 6))
     dados_retornos_completo = {}
     for ativo, df in dados_ativos.items():
-        # Calcular os retornos para cada ativo
-        df_retornos = (df['Close'].pct_change() + 1).cumprod() - 1
-        dados_retornos_completo[ativo] = df_retornos  
-        ax_retornos.plot(df_retornos.index, df_retornos, label=f"{ativo}")       
+        # Calcular os retornos apenas se houver dados disponíveis
+        if len(df) > 1:
+            df_retornos = (df['Close'].pct_change() + 1).cumprod() - 1
+            dados_retornos_completo[ativo] = df_retornos  
+            ax_retornos.plot(pd.to_datetime(df_retornos.index), df_retornos, label=f"{ativo}")
+
+    # Adicionando legenda e título
+    if selected_indice == "IPCA":
+        ax_retornos.plot(pd.to_datetime(df_ipca.index), df_retornos_ipca, label="IPCA")
+    elif selected_indice == "CDI":
+        ax_retornos.plot(pd.to_datetime(df_cdi.index), df_retornos_cdi, label="CDI")
+    elif selected_indice == "TAXA SELIC":
+        ax_retornos.plot(pd.to_datetime(df_selic.index), df_retornos_selic, label="SELIC")
         # Adicionando legenda e título
-        plt.legend()
-        plt.title("Comparação de Rendimento de Ativos")
-        plt.xlabel('Data')
-        plt.ylabel('Rendimento')
+    plt.legend()
+    plt.title("Comparação de Rendimento de Ativos")
+    plt.xlabel('Data')
+    plt.ylabel('Rendimento')
    # Exibindo o gráfico de retornos
     st.pyplot(fig_retornos)
 
@@ -130,9 +157,9 @@ for ativo, df in dados_ativos.items():
         st.write(f"**Baixa do último dia disponível:** R$ {last_data['Low']:.2f}")
         st.write(f"**Fechamento do último dia disponível:** R$ {last_data['Close']:.2f}")
         if rendimento_total < 0:
-            st.write(f"**Rendimento:** <span style='color:{color_negative}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
+            st.write(f"**Rendimento no período:** <span style='color:{color_negative}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
         else:
-            st.write(f"**Rendimento:** <span style='color:{color_positive}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
+            st.write(f"**Rendimento no período:** <span style='color:{color_positive}'>{rendimento_total:.2%}</span>", unsafe_allow_html=True)
     else:
         st.write("Não há dados suficientes para calcular retornos.")
     
