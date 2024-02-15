af0c3985392129d7ee77ff87770321ebe9da2b86
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-02-15 15:47:14 -0300

    Update calculadorajc.py

--- calculadorajc.py
+++ calculadorajc.py
@@ -5,7 +5,7 @@ st.header("Calculadora de Juros Compostos")
 capital = st.text_input("Valor Inicial:")
 capital = float(capital) if capital else 0.0
 juros = st.number_input("Taxa de Juros:", min_value=0.5, step=0.5, format="%g")
-tempo = st.number_input("Tempo (mês):", min_value=1, step=1)
+tempo = st.number_input("Tempo (ano):", min_value=1, step=1)
 
 montante = capital * (1 + (juros / 100) ** tempo)
 valor_juros = montante - capital
