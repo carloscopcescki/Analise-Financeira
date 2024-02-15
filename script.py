9df0b2c06a953435eb10e7cd776872d201859099
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-02-15 08:48:29 -0300

    Update script.py

--- script.py
+++ script.py
@@ -182,11 +182,11 @@ valor_ativo = {}
 for ativo in selected_ativos:
     
     # Obter o total de proventos em 1 ano e dividend yield
-    valor_ativo = df.iloc[-1]
+    valor_ativo = call_api['Close'].iloc[-1]
     somatoria_dy = somatoria_por_ano.tail(2)
     somatoria_dy = somatoria_por_ano.iloc[-1]
     total_provento = somatoria_dy['Valor'].sum()
-    dividend_yield = (total_provento / valor_ativo['Close']) * 100
+    dividend_yield = (total_provento / valor_ativo) * 100
     
     # Construir a URL dinâmica para cada ativo
     stock_url = f'https://www.dadosdemercado.com.br/bolsa/acoes/{ativo}/dividendos'
