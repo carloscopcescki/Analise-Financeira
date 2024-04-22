dcaf8ffea3c50a7e71a51060934ace4d72560b9a
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-04-22 16:03:19 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1029,7 +1029,7 @@ with aba3:
     for _ in range(tempo_anos_mes):
         montante = capital + aporte_mensal
         montante *= (1 + taxa_juros)
-        montante = capital
+        capital = montante
     
     juros_total = montante - capital - (aporte_mensal * tempo_anos)
     
