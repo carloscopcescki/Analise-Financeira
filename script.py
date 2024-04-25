a0d00568d5e2f1f4b4ce5a95b9d5cf4feaac9f10
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-04-25 11:46:28 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1039,12 +1039,10 @@ with aba3:
         
     deposito = deposito + capital
     juros_compostos = total - deposito
-
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
     
-    deposito_formatado = locale.currency(deposito, grouping=True, symbol='R$ ')
-    juros_formatado = locale.currency(juros_compostos, grouping=True, symbol='R$ ')
-    total_formatado = locale.currency(total, grouping=True, symbol='R$ ')
+    deposito_formatado = "R$ {:,.2f}".format(deposito).replace('.', '.')
+    juros_formatado = "R$ {:,.2f}".format(juros_compostos).replace('.', '.')
+    total_formatado = "R$ {:,.2f}".format(total).replace('.', '.')
     
     if st.button("Calcular", key="calcular_button"):
         st.write(f"**Total Investido:** R$ {deposito_formatado}")
