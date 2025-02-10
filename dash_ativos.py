3535a4a834d9f27d3b7828a26a55098dc630d390
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2025-02-10 11:11:41 -0300

    Update dash_ativos.py

--- dash_ativos.py
+++ dash_ativos.py
@@ -2,7 +2,6 @@ from functions import *
 from fundamentals import Fundamental
 import streamlit as st
 from streamlit_extras.metric_cards import style_metric_cards
-from streamlit_extras.grid import grid
 
 stock_dashboard = Page()
 stock_dashboard.webpage()
@@ -11,53 +10,54 @@ stock_dashboard.webpage()
 markets = ['Ações', 'Fundos Imobiliários']
 
 indices = {
-        'BOVESPA': '^BVSP',
-        'IBRX 50': '^IBV50',
-        'DÓLAR': 'BRL=X',
-        'EURO': 'EURBRL=X',
-        'S&P 500': '^GSPC',
-        'DOW JONES': '^DJI',
-        'NASDAQ': '^IXIC',
+    'BOVESPA': '^BVSP',
+    'IBRX 50': '^IBV50',
+    'DÓLAR': 'BRL=X',
+    'EURO': 'EURBRL=X',
+    'S&P 500': '^GSPC',
+    'DOW JONES': '^DJI',
+    'NASDAQ': '^IXIC',
 }
 
 # Tipos e ativos
-get_market = str(st.sidebar.selectbox("Escolha um tipo de renda variável", [''] + markets))
+get_market = str(
+    st.sidebar.selectbox("Escolha um tipo de renda variável", [''] + markets))
 if get_market:
-    try:     
+    try:
         market = Market(get_market)
         stock_list = market.stock_list()
-        stock = str(st.sidebar.selectbox("Escolha um ativo", [''] + stock_list))
+        stock = str(st.sidebar.selectbox("Escolha um ativo",
+                                         [''] + stock_list))
         if stock:
             stock_data = market.stock_data(stock)
-            fundamental_data = Fundamental(stock)    
+            fundamental_data = Fundamental(stock)
             name = fundamental_data.name()
             dy = fundamental_data.dividend_yield()
             price = fundamental_data.price()
             pl = fundamental_data.pl()
             pvp = fundamental_data.pvp()
-            
-            my_grid = grid(2)
-            c = my_grid.container(border=True)
-            colImage, colStock = c.columns(2)
-            
-            with colImage:
-                if stock != "ISAE4" and stock != "ISAE3":
-                    img = st.image(f"https://raw.githubusercontent.com/thecartera/B3-Assets-Images/refs/heads/main/imgs/{stock}.png", width=100)
-                else:
-                    img = st.image(f"https://raw.githubusercontent.com/thecartera/B3-Assets-Images/refs/heads/main/imgs/TRPL4.png", width=100)
-            with colStock:    
+
+            col_img, col_name = st.columns(2)
+
+            with col_img:
+                img = st.image(
+                    f"https://raw.githubusercontent.com/thecartera/B3-Assets-Images/refs/heads/main/imgs/{stock}.png",
+                    width=85)
+            with col_name:
                 st.header(name)
-            
+
             col1, col2, col3, col4 = st.columns(4)
-            
+
             col1.metric(label="Valor da cotação", value=f"R$ {price}")
             col2.metric(label="Dividend Yield", value=dy)
-            col3.metric(label="P/L", value=pl)
+            col3.metric(label="PL", value=pl)
             col4.metric(label="PVP", value=pvp)
-            
-            style_metric_cards(background_color="#0E1117",
-                    border_left_color='#afb381',
-                    border_color='#afb381')
+
+            style_metric_cards(border_left_color='#0047AB',
+                               border_color='#0047AB')
+
+            market.price_chart(stock)
+            market.dividends_chart(stock)
             
             st.dataframe(stock_data, width=850, height=350)
             st.dataframe(fundamental_data.table(stock), width=850, height=350)
